86f6b233fc168aa6147822172a56990c03e66ea0
mmasu012 <mmasu012@student.ucr.edu>
2024-02-20 22:37:17 +0000

    hide github icon and fork option

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ def inject_custom_css():
             background-color: #dbdce4;
             
         }*/
-        #MainMenu {
-          visibility: hidden;
+        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+        .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+        .viewerBadge_text__1JaDK {
+            display: none;
         }
     </style>
     """
